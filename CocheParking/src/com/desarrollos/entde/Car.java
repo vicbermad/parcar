efca5e43f181b6473ff6441b926e1f2a67177bf6
vicbermad <vicbermad@alu.edu.gva.es>
2023-01-14 13:41:01 +0100

    master

--- CocheParking/src/com/desarrollos/entde/Car.java
+++ CocheParking/src/com/desarrollos/entde/Car.java
@@ -33,7 +33,7 @@ public class Car
         slowDown();
         moveDown();
         accelerate();
-        car.printGarage();
+        Garage.printGarage();
 //        turnRight();
 //        turnHeadlightsOn();
 //        turnHeadlightsOff();
@@ -43,7 +43,7 @@ public class Car
      * Accelerate the car
      * @throws InterruptedException 
      */
-    public void accelerate() throws InterruptedException{
+    public static void accelerate() throws InterruptedException{
         String c = new String("*");
         String space = new String(" ");
     	for(int i=0; i<5; i++)
@@ -58,8 +58,8 @@ public class Car
      * Slow down the car
      * @throws InterruptedException 
      */
-    public void slowDown() throws InterruptedException{
-    	string c = new string("     *");
+    public static void slowDown() throws InterruptedException{
+    	String c = new String("     *");
     	for(int i=0; i<5; i++)
         {
         	System.out.print(c);
@@ -73,9 +73,9 @@ public class Car
      * Move the car downwards
      * @throws InterruptedException 
      */    
-    public void moveDown() throws InterruptedException{
-    	string c = new string("\t\t\t\t  *"));
-    	for(int i=0, i<5, i++)
+    public static void moveDown() throws InterruptedException{
+    	String c = new String("\t\t\t\t  *");
+    	for(int i=0; i<5; i++)
         {
     		if(i!=4)
     			System.out.println(c);
--- CocheParking/src/com/desarrollos/entde/Garage.java
+++ CocheParking/src/com/desarrollos/entde/Garage.java
@@ -2,7 +2,7 @@ package com.desarrollos.entde;
 
 public class Garage{
     
-    public void printGarage() 
+    public static void printGarage() 
     {
         system.out.print("|*]");
     }
